7366ee3091e095772a64eb80cfbc4f7043950fce
smaru1111 <96244711+smaru1111@users.noreply.github.com>
2022-09-28 03:10:30 +0900

    #16 add playボタンの作成

--- src/page/Start.js
+++ src/page/Start.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
+import { Button } from "../components/Button";
+
 import "../css/Start.css";
 
 export const Start = () => {
@@ -10,6 +12,9 @@ export const Start = () => {
       <div className="body-container">
         <p className="body-subtitle">LET'S EXPLORE</p>
         <p className="body-title">THE SAGA</p>
+        <div className="body-button">
+          <Button name="play" path="/Game" />
+        </div>
       </div>
     </main>
   );
