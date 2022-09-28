9f552307c449c41f197ca11076645817e7209c97
ryutaro-kimura <sasuker2264@gmail.com>
2022-09-28 19:37:20 +0900

    #21 add クリック時の座標を取得

--- src/components/GoogleMapComponent.js
+++ src/components/GoogleMapComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const containerStyle = {
@@ -11,16 +11,25 @@ const center = {
   lng: 130.2903679388433
 };
 
-const targetPosition = {
-  lat: 33.24490720899812,
-  lng: 130.29212694850503
-};
+const GoogleMapComponent = ({ setSubmitPosition }) => {
+  const [pin, setPin] = useState();
 
-const GoogleMapComponent = () => {
+  const setLatLng = props => {
+    const pos = {
+      lat: props.latLng.lat(),
+      lng: props.latLng.lng()
+    };
+    setPin(pos);
+    setSubmitPosition(pos);
+  };
   return (
     <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17}>
-        <Marker position={targetPosition} />
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={17}
+        onClick={setLatLng}>
+        {pin && <Marker position={pin} />}
       </GoogleMap>
     </LoadScript>
   );
--- src/page/Game.js
+++ src/page/Game.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import GoogleMapComponent from "../components/GoogleMapComponent";
 
 export const Game = () => {
+  const [submitPosition, setSubmitPosition] = useState();
+  console.log("subm", submitPosition);
   return (
     <>
       <h1>ゲーム画面</h1>
       <Link to='/'>スタート画面に戻る</Link>
       <br></br>
       <Link to='/Result'>結果をみる</Link>
-      <GoogleMapComponent />
+      <GoogleMapComponent setSubmitPosition={setSubmitPosition} />
     </>
   );
 };
