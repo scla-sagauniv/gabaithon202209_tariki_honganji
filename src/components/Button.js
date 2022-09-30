38ccdf099aaec77dc1d2b9d7ce64f4968c29a040
ryutaro-kimura <sasuker2264@gmail.com>
2022-10-01 02:33:08 +0900

    #42 add ホームに戻るボタンの作成

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../css/button.css";
 
-export const Button = ({ name, path }) => {
+export const Button = ({ name, path, onClick }) => {
   return (
     <>
-      <Link to={path} className="button-all">
+      <Link to={path} className='button-all'>
         {name}
       </Link>
     </>
