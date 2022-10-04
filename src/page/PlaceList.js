cb41304e9114f5615fa03141c485fd20c8850501
ryutaro-kimura <sasuker2264@gmail.com>
2022-10-04 11:38:45 +0900

    add: 戻るボタンもつけちゃう

--- src/page/PlaceList.js
+++ src/page/PlaceList.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Button } from "../components/Button";
+import { Navbar } from "../components/Navbar";
 import StickyHeadTable from "../components/StickyHeadTable";
 
 const PlaceList = () => {
   return (
     <>
+      <Navbar />
       <StickyHeadTable />
+      <div style={{marginLeft: "auto", width: "160px", marginTop: "32px"}}>
+        <Button name='back' path='/' />
+      </div>
     </>
   );
 };
